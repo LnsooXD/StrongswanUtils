34ab724e1d9726d03f41bfd2627ca83e583fc06a
LnsooXD <LnsooXD@gmail.com>
2019-09-18 02:50:46 +0800

    add scripts

---
+++ client_key_gen.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+SS_CA_PRIVATE_KEY_PATH="private/strongswan-ca-private-key.pem"
+SS_CA_CERT_PATH="cacerts/strongswan-ca-cert.pem"
+
+# Parse organization from CA certificate.
+SS_CA_O=`strongswan pki \
+	--print \
+	--in "${SS_CA_CERT_PATH}" \
+	| \
+	awk -F '"' '/subject:/{ret = index($2, "C=");ret=substr($2,ret);split(ret,arr,",");split(arr[1],arr,"=");print arr[2]}'
+`
+# Parse CA common name from CA certificate.
+SS_CA_CN=`strongswan pki \
+	--print \
+	--in "${SS_CA_CERT_PATH}" \
+	| \
+	awk -F '"' '/subject:/{ret = index($2, "CN=");ret=substr($2,ret);split(ret,arr,",");split(arr[1],arr,"=");print arr[2]}'
+`
+
+
+KEY_SIZE_DEFAULT=4096
+KEY_LIFETIME_DEFAULT=3650
+
+DN_DEFAULT_C=CN
+
+CLIENT_DEFAULT_USER_NAME="User"
+CLIENT_DEFAULT_USER_EMAIL="user@example.com"
+
+read -p "Key size[${KEY_SIZE_DEFAULT}]:" KEY_SIZE
+read -p "Key lifetime (Days)[${KEY_LIFETIME_DEFAULT}]:" KEY_LIFETIME
+read -p "Country[${DN_DEFAULT_C}]:" DN_C
+read -p "User name[${CLIENT_DEFAULT_USER_NAME}]:" CLIENT_USER_NAME
+read -p "User email[${CLIENT_DEFAULT_USER_EMAIL}]:" CLIENT_USER_EMAIL
+
+KEY_SIZE=${KEY_SIZE:-${KEY_SIZE_DEFAULT}}
+KEY_LIFETIME=${KEY_LIFETIME:-${KEY_LIFETIME_DEFAULT}}
+DN_C=${DN_C:-${DN_DEFAULT_C}}
+CLIENT_USER_NAME=${CLIENT_USER_NAME:-${CLIENT_DEFAULT_USER_NAME}}
+CLIENT_USER_EMAIL=${CLIENT_USER_EMAIL:-${CLIENT_DEFAULT_USER_EMAIL}}
+
+echo 
+echo
+echo "=========================== Information ==========================="
+echo "Key size: ${KEY_SIZE}"
+echo "Key lifetime: ${KEY_LIFETIME} (Days)"
+echo "Organization: ${SS_CA_O}"
+echo "CA common name: ${SS_CA_CN}"
+echo "User name: ${CLIENT_USER_NAME}"
+echo "User email: ${CLIENT_USER_EMAIL}"
+echo "==================================================================="
+
+echo 
+echo
+echo "Generating client keys and certificates for ${CLIENT_USER_NAME}..."
+echo
+echo
+
+mkdir -p private && \
+mkdir -p cacerts && \
+mkdir -p certs &&\
+mkdir -p p12-certs
+
+
+CLIENT_PRIVATE_KEY_PATH="private/strongswan-client-${CLIENT_USER_NAME}-private-key.pem"
+CLIENT_CERT_PATH="certs/strongswan-client-${CLIENT_USER_NAME}-cert.pem"
+CLIENT_CERT_P12_CERT_PATH="p12-certs/strongswan-client-${CLIENT_USER_NAME}-cert.p12"
+
+strongswan pki \
+	--gen \
+	--type rsa \
+	--size ${KEY_SIZE} \
+	--outform pem \
+	> "${CLIENT_PRIVATE_KEY_PATH}"
+
+strongswan pki \
+	--pub \
+	--in "${CLIENT_PRIVATE_KEY_PATH}" \
+	--type rsa \
+	--outform pem \
+	| \
+strongswan pki \
+	--issue \
+	--lifetime ${KEY_LIFETIME} \
+	--cakey "${SS_CA_PRIVATE_KEY_PATH}" \
+	--cacert "${SS_CA_CERT_PATH}" \
+	--dn "C=${DN_C}, O=${SS_CA_O}, CN=${CLIENT_USER_EMAIL}" \
+	--san ${CLIENT_USER_EMAIL} \
+	--outform pem \
+	> "${CLIENT_CERT_PATH}" \
+
+echo "Client certificate for ${CLIENT_USER_NAME}:"
+strongswan pki \
+	--print \
+	--in "${CLIENT_CERT_PATH}"
+echo
+
+echo "Enter password to protect p12 cert for ${CLIENT_USER_NAME}"
+openssl pkcs12 \
+	-export \
+	-inkey "${CLIENT_PRIVATE_KEY_PATH}" \
+	-in "${CLIENT_CERT_PATH}" \
+	-name "${CLIENT_USER_NAME}'s VPN Certificate of ${SS_CA_CN}" \
+	-certfile "${SS_CA_CERT_PATH}" \
+	-caname "${SS_CA_CN}" \
+	-out "${CLIENT_CERT_P12_CERT_PATH}"
---
+++ server_key_gen.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+
+SS_CA_PRIVATE_KEY_PATH="private/strongswan-ca-private-key.pem"
+SS_CA_CERT_PATH="cacerts/strongswan-ca-cert.pem"
+
+SS_SERVER_PRIVATE_KEY_PATH="private/strongswan-server-private-key.pem"
+SS_SERVER_CERT_PATH="certs/strongswan-server-cert.pem"
+
+KEY_SIZE_DEFAULT=4096
+KEY_LIFETIME_DEFAULT=3650
+
+DN_DEFAULT_C=CN
+DN_DEFAULT_O=Lnsoo
+DN_DEFAULT_CA_CN="Lnsoo org"
+DN_DEFAULT_CN=0.0.0.0
+
+read -p "Key size[${KEY_SIZE_DEFAULT}]:" KEY_SIZE
+read -p "Key lifetime (Days)[${KEY_LIFETIME_DEFAULT}]:" KEY_LIFETIME
+read -p "Country[${DN_DEFAULT_C}]:" DN_C
+read -p "Organization[${DN_DEFAULT_O}]:" DN_O
+read -p "CA common name[${DN_DEFAULT_CA_CN}]:" DN_CA_CN
+read -p "IP Or Domain[${DN_DEFAULT_CN}]:" DN_CN
+
+KEY_SIZE=${KEY_SIZE:-${KEY_SIZE_DEFAULT}}
+KEY_LIFETIME=${KEY_LIFETIME:-${KEY_LIFETIME_DEFAULT}}
+DN_C=${DN_C:-${DN_DEFAULT_C}}
+DN_O=${DN_O:-${DN_DEFAULT_O}}
+DN_CA_CN=${DN_CA_CN:-${DN_DEFAULT_CA_CN}}
+DN_CN=${DN_CN:-${DN_DEFAULT_CN}}
+
+echo 
+echo
+echo "=========================== Information ==========================="
+echo "Key size: ${KEY_SIZE}"
+echo "Key lifetime: ${KEY_LIFETIME} (Days)"
+echo "Country: ${DN_C}"
+echo "Organization: ${DN_O}"
+echo "CA common name: ${DN_CA_CN}"
+echo "IP Or Domain: ${DN_CN}"
+echo "==================================================================="
+
+echo 
+echo
+echo "Generating CA certificate ..."
+
+mkdir -p private && \
+mkdir -p cacerts && \
+mkdir -p certs \
+
+# Generate a private key for CA certificate.
+strongswan pki \
+    --gen \
+    --type rsa \
+    --size ${KEY_SIZE} \
+    --outform pem \
+    > "${SS_CA_PRIVATE_KEY_PATH}" \
+
+# Generate a self-signed CA certificate with the CA private key.
+strongswan pki \
+    --self \
+    --ca \
+    --lifetime ${KEY_LIFETIME} \
+    --in "${SS_CA_PRIVATE_KEY_PATH}" \
+    --type rsa \
+    --dn "C=${DN_C}, O=${DN_O}, CN=${DN_CA_CN}" \
+    --outform pem \
+    > "${SS_CA_CERT_PATH}" \
+
+echo "CA certificate at ${SS_CA_CERT_PATH}:"
+strongswan pki \
+    --print \
+    --in "${SS_CA_CERT_PATH}" \
+
+echo 
+echo
+sleep 1
+echo "Generating server keys ..."
+echo 
+echo
+
+# Generate a private key for server certificate.
+strongswan pki \
+    --gen \
+    --type rsa \
+    --size ${KEY_SIZE} \
+    --outform pem \
+    > "${SS_SERVER_PRIVATE_KEY_PATH}" \
+
+# Create a server certificate with the server private key signed by the CA certificate.
+strongswan pki \
+    --pub \
+    --type rsa \
+    --in "${SS_SERVER_PRIVATE_KEY_PATH}" \
+    --outform pem \
+    | \
+strongswan pki \
+    --issue \
+    --lifetime ${KEY_LIFETIME_DEFAULT} \
+    --cakey "${SS_CA_PRIVATE_KEY_PATH}" \
+	--cacert "${SS_CA_CERT_PATH}" \
+	--dn "C=${DN_C}, O=${DN_O}, CN=${DN_CN}" \
+	--san ${DN_CN} \
+	--flag serverAuth \
+    --flag ikeIntermediate \
+	--outform pem \
+    > "${SS_SERVER_CERT_PATH}" \
+
+echo "VPN server cert at ${SS_SERVER_CERT_PATH}:"
+strongswan pki \
+    --print \
+    --in "${SS_SERVER_CERT_PATH}"
